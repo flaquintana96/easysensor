b3afa93244bcc3ffd9422c25f53c7a4d124810ab
flaquintana96 <31343054+flaquintana96@users.noreply.github.com>
2017-08-25 20:21:45 +0200

    apici

--- alert.php
+++ alert.php
@@ -1,8 +1,8 @@
  <?php
  
 function Alert($mex){
-  $pre="<script> alert(";
- 	$post="); </script>";
+  $pre='<script> alert(';
+ 	$post='); </script>';
   $stampa=$pre.$mex.$post;
  	echo $stampa;
   }
